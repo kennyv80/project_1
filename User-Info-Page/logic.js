27296d3aeb1886459d25ba14da9ce074a116fa6d
quylai <quylai@gmail.com>
2019-07-14 18:32:44 -0500

    fixed ID for State

---
+++ User-Info-Page/logic.js
@@ -0,0 +1,16 @@
+var config = {
+  apiKey: "AIzaSyDYLpe8h2FUgrC8HbkY3VuyrFhJLb2T8cE",
+  authDomain: "mytestproj-5b683.firebaseapp.com",
+  databaseURL: "https://mytestproj-5b683.firebaseio.com",
+  projectId: "mytestproj-5b683",
+  storageBucket: "",
+  messagingSenderId: "87679041814",
+  appId: "1:87679041814:web:75dabb1bed76630e"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+var nameZ = "";
+var emailZ = "";
+var ageZ = 0;
+var commentZ = "";
