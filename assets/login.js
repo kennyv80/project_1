f457802773c81b0c56c921d2f6504c2eb3158e58
quylai <quylai@gmail.com>
2019-07-19 20:25:07 -0500

    added validation

---
+++ assets/login.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    $('#submit-btn').on('click', function() {
+        console.log('test');
+//var form = $(".form-signin");
+//form.validate()
+//$('#submit-btn').on('click', function() {
+    //console.log('test');
+    //if ($('.form-signin').valid()) {
+        //$(this).button('loading');
+    //}
+    //return true;
+
+    });
+});
+
+//link to Firebase... check with team for credentials
+//CORS - cross origin reference sharing - enable to call to more than one API at once (Chrome add-on)
